99e9a7bac9ae3f0200b6febf91dcfa5d7d437102
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-09-08 09:01:54 -0300

    clear whole application

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import TestComponent from "../TestComponent";
-// import ClassComponent from "../ClassComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <TestComponent name="Caio" />
-        {/* <ClassComponent name="World" /> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">App</div>;
 }
 
 export default App;
