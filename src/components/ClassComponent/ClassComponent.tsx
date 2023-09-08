34966dc9f24311c874ae8976800767db09298cce
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-09-08 08:25:55 -0300

    ract life cycle

--- src/components/ClassComponent/ClassComponent.tsx
+++ src/components/ClassComponent/ClassComponent.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
 class ClassComponent extends React.Component<{ name: string }> {
+  constructor(props: any) {
+    super(props);
+    console.log("Constructor reached");
+  }
+
   state = {
     name: "World",
   };
 
+  componentDidMount(): void {
+    console.log("did mount reached");
+  }
+
+  componentDidUpdate(): void {
+    console.log("did update reached")
+  }
+
   render(): React.ReactNode {
+    console.log("Render reached");
     return (
       <div>
         <p>name: {this.state.name}</p>
