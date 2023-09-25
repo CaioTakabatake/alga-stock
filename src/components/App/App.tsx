5cca6c16ffe321835b1ebb0ba7f393c2c6b75256
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-09-25 13:53:36 -0300

    add table actions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -38,7 +38,14 @@ function App() {
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Table headers={headers} data={products} />
+        <Table
+          headers={headers}
+          data={products}
+          enableActions
+          onDelete={console.log}
+          onDetail={console.log}
+          onEdit={console.log}
+        />
         <ProductForm
           form={updatingProduct}
           onSubmit={handleProductSubmit}
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Table.scss";
 import organizeData from "../../utils/organizeDataForTable";
+import Button from "../Button";
 
 export interface TableHeader {
   key: string;
@@ -33,6 +34,7 @@ const Table: React.FC<TableProps> = (props) => {
               {header.value}
             </th>
           ))}
+          {props.enableActions && <th className="right">Actions</th>}
         </tr>
       </thead>
       <tbody>
@@ -48,6 +50,30 @@ const Table: React.FC<TableProps> = (props) => {
                     {row[item]}
                   </td>
                 ) : null
+              )}
+
+              {props.enableActions && (
+                <td className="actions right">
+                  {props.onEdit && (
+                    <Button onClick={() => props.onEdit && props.onEdit(row)}>
+                      Edit
+                    </Button>
+                  )}
+                  {props.onDetail && (
+                    <Button
+                      onClick={() => props.onDetail && props.onDetail(row)}
+                    >
+                      Detail
+                    </Button>
+                  )}
+                  {props.onDelete && (
+                    <Button
+                      onClick={() => props.onDelete && props.onDelete(row)}
+                    >
+                      Delete
+                    </Button>
+                  )}
+                </td>
               )}
             </tr>
           );
