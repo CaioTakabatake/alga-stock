f6ccf3d49e50b66b89db71bdac980220a9782b3b
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-09-25 11:11:24 -0300

    populate table

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,13 +3,22 @@ import "./App.css";
 import Header from "../Header/Header";
 import Container from "../../shared/Container";
 import Table from "../../shared/Table";
+import { TableHeader } from "../../shared/Table";
+import Products from "../../shared/Table/Table.mockdata";
+
+const headers: TableHeader[] = [
+  { key: "id", value: "#" },
+  { key: "Actions", value: "Actions" },
+  { key: "price", value: "Price", right: true },
+  { key: "stock", value: "Available Stock", right: true },
+];
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Table />
+        <Table headers={headers} data={Products} />
       </Container>
     </div>
   );
--- src/shared/Table/Table.tsx
+++ src/shared/Table/Table.tsx
@@ -1,62 +1,34 @@
 import React from "react";
 import "./Table.scss";
-import Products from "./Table.mockdata";
+import organizeData from "../../utils/organizeDataForTable";
 
-const headers: TableHeader[] = [
-  { key: "id", value: "#" },
-  { key: "Actions", value: "Actions" },
-  { key: "price", value: "Price", right: true },
-  { key: "stock", value: "Available Stock", right: true },
-];
-
-declare interface TableHeader {
+export interface TableHeader {
   key: string;
   value: string;
   right?: boolean;
 }
 
-type IndexedHeaders = {
-  [key: string]: TableHeader;
-};
+declare interface TableProps {
+  headers: TableHeader[];
+  data: any[];
 
-type OrganizedItem = {
-  [key: string]: any;
-};
+  enableActions?: boolean;
 
-function organizeData(
-  data: any[],
-  headers: TableHeader[]
-): [OrganizedItem[], IndexedHeaders] {
-  const indexedHeaders: IndexedHeaders = {};
-
-  headers.forEach((header) => {
-    indexedHeaders[header.key] = header;
-  });
-
-  const headerKeysInOrder = Object.keys(indexedHeaders);
-
-  const organizedData = data.map((item) => {
-    const organizedItem: OrganizedItem = {};
-
-    headerKeysInOrder.forEach((key) => {
-      organizedItem[key] = item[key];
-    });
-
-    organizedItem.$original = item;
-
-    return organizedItem;
-  });
-
-  return [organizedData, indexedHeaders];
+  onDelete?: (item: any) => void;
+  onDetail?: (item: any) => void;
+  onEdit?: (item: any) => void;
 }
 
-const Table = () => {
-  const [organizedData, indexedHeaders] = organizeData(Products, headers);
+const Table: React.FC<TableProps> = (props) => {
+  const [organizedData, indexedHeaders] = organizeData(
+    props.data,
+    props.headers
+  );
   return (
     <table className="AppTable">
       <thead>
         <tr>
-          {headers.map((header) => (
+          {props.headers.map((header) => (
             <th className={header.right ? "right" : ""} key={header.key}>
               {header.value}
             </th>
