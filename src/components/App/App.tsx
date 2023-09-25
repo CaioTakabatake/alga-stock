96ce8162a94d460ec07a7b9e57baa99a10fadf54
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-09-25 08:28:59 -0300

    clear app + add table component's base file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,30 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Header from "../Header/Header";
-import Button from "../../shared/Button/Button";
 import Container from "../../shared/Container";
-import Input from "../../shared/Input";
-
-function TestComponent() {
-  return (
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/64/64673.png"
-      alt="search icon"
-    />
-  );
-}
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
-      <Container>
-        <ul>
-          {["Daniel", "Caio", "Thiago", "Caio"].map((name, index) => {
-            return <li key={index}>{name}</li>;
-          })}
-        </ul>
-      </Container>
+      <Container></Container>
     </div>
   );
 }
---
+++ src/shared/Table/Table.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Table.scss";
+
+const Table = () => {
+  return <table></table>;
+};
+
+export default Table;
