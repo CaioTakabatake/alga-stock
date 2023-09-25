f4407bf5d120302e7bc0002991192c4c2b48aadf
CaioTakabatake <caio.soldera.taka@gmail.com>
2023-09-25 12:39:00 -0300

    product submit working

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,26 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header";
 import Container from "../../shared/Container";
 import Table from "../../shared/Table";
 import { TableHeader } from "../../shared/Table";
 import Products from "../../shared/Table/Table.mockdata";
-import ProductForm from "../Products/ProductForm";
+import ProductForm, { ProductCreator } from "../Products/ProductForm";
 
 const headers: TableHeader[] = [
   { key: "id", value: "#" },
-  { key: "Actions", value: "Actions" },
+  { key: "name", value: "Product" },
   { key: "price", value: "Price", right: true },
   { key: "stock", value: "Available Stock", right: true },
 ];
 
 function App() {
+  const [products, setProducts] = useState(Products);
+
+  const handleProductSubmit = (product: ProductCreator) => {
+    setProducts([...products, { id: products.length + 1, ...product }]);
+  };
+
   return (
     <div className="App">
       <Header title="AlgaStock" />
       <Container>
-        <Table headers={headers} data={Products} />
-        <ProductForm />
+        <Table headers={headers} data={products} />
+        <ProductForm onSubmit={handleProductSubmit} />
       </Container>
     </div>
   );
--- src/components/Products/ProductForm.tsx
+++ src/components/Products/ProductForm.tsx
@@ -9,7 +9,17 @@ const initialFormState = {
   stock: "",
 };
 
-const ProductForm = () => {
+export interface ProductCreator {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+declare interface ProductFormProps {
+  onSubmit: (product: ProductCreator) => void;
+}
+
+const ProductForm: React.FC<ProductFormProps> = (props) => {
   const [form, setForm] = useState(initialFormState);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -21,10 +31,22 @@ const ProductForm = () => {
     });
   };
 
+  const handleFormSubmit = () => {
+    const productDto = {
+      name: String(form.name),
+      price: parseFloat(form.price),
+      stock: Number(form.stock),
+    };
+
+    props.onSubmit(productDto);
+    setForm(initialFormState);
+  };
+
   return (
-    <Form onSubmit={() => console.log(form)}>
+    <Form onSubmit={handleFormSubmit}>
       <Input
         onChange={handleInputChange}
+        value={form.name}
         name="name"
         label="Name"
         placeholder="Ex: Cookie"
@@ -32,6 +54,7 @@ const ProductForm = () => {
       />
       <Input
         onChange={handleInputChange}
+        value={form.price}
         name="price"
         label="Price"
         type="number"
@@ -42,6 +65,7 @@ const ProductForm = () => {
       />
       <Input
         onChange={handleInputChange}
+        value={form.stock}
         name="stock"
         label="Stock"
         type="number"
